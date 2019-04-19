edaef3f9bd986f496b4e574ebd94fecf3237e2c4
Dike <2675549107@qq.com>
2019-04-19 11:42:42 +0800

    no message

--- src/test/java/pipline_test/MyUtilsTest.java
+++ src/test/java/pipline_test/MyUtilsTest.java
@@ -1,39 +1,38 @@
-/**
- * 
- */
 package pipline_test;
 
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 import org.junit.Assert;
-import org.junit.Test;
 
-/**
- * @ClassName MyUtilsTest
- *
- * @Description
- * 
- * @Author Dike
- *
- * @Date 2019年4月19日 上午10:25:03
- */
+import org.testng.annotations.AfterClass;
+
 public class MyUtilsTest {
-	private MyUtils myUtils = new MyUtils();
+  private MyUtils myUtils = new MyUtils();
 	
-	@Test
-	public void testAdd() {
-		Assert.assertEquals(3, myUtils.add(1, 2), 0.00);
-		
-		Assert.assertEquals(15, myUtils.add(12, 3), 0.00);
-	}
-	
-	@Test
-	public void testSub() {
-		Assert.assertEquals(1, myUtils.sub(16, 15), 0.00);
-		
-		Assert.assertEquals(6, myUtils.sub(26, 20), 0.00);
-	}
-	
-	@Test
-	public void testMul() {
-		Assert.assertEquals(6, myUtils.mul(2, 3), 0.00);
-	}
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+  @Test
+  public void testAdd() {
+	  Assert.assertEquals(3, myUtils.add(1, 2), 0.00);
+	  
+	  Assert.assertEquals(9, myUtils.add(3, 6), 0.00);
+  }
+  
+  @Test
+  public void testSub() {
+	  Assert.assertEquals(1, myUtils.sub(3, 2), 0.00);
+	  
+	  Assert.assertEquals(6, myUtils.sub(26, 20), 0.00);
+  }
+  
+  @Test
+  public void testMul() {
+	  Assert.assertEquals(6, myUtils.mul(3, 2), 0.00);
+  }
 }
